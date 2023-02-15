f9fd7a7718bfd7744d2b6b2960771bbe43d0405d
mynttiti <timo.mynttinen@xamk.fi>
2023-02-15 14:50:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using OOPTask6C;
+Student student;
+
+//All the student information:
+List<Student> students = new List<Student>();
+bool moreStudents;
+string received;
+string sName;
+
+do
+{
+    Console.Write("Enter student name: ");
+    sName = Console.ReadLine();
+
+    //Create a Student object
+    student=new Student(Student.ProvideUniqueIdNumber(), sName);
+    //Add to your collection:
+    students.Add(student);
+
+    Console.Write("Continue to enter student information Y/N?: ");
+    received = Console.ReadLine().ToUpper();
+    if(received.StartsWith("Y"))
+        moreStudents= true;
+    else
+        moreStudents= false;
+
+} while (moreStudents);
+
+//Situation at the end:
+foreach (var item in students)
+{
+    Console.WriteLine("Id: "+item.Id+", student name: "+
+        item.StudentName);
+}
---
+++ Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPTask6C
+{
+    internal class Student
+    {
+        public static int index=0;
+        public Student(int id, string studentName)
+        {
+            Id = id;
+            StudentName = studentName;
+        }
+
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+
+        //Static method
+        public static int ProvideUniqueIdNumber()
+        {
+            return ++index;
+        }
+
+
+    }
+}
